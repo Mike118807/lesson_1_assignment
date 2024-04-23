cf778fcb2e0fa67838cba6164d9ec78fd55ba878
Michael Davis <mike118807@icloud.com>
2024-04-23 14:58:31 -0700

    first commit

---
+++ Indentation_practice_if_statemants.py
@@ -0,0 +1,52 @@
+
+# Problem 1
+
+weather = " sunny "
+
+if weather == "sunny" :
+    print ( "Wear sunglassed!" )
+else:
+    print ( "Take an umbrella!" )
+
+# Problem 2
+
+
+pi_value = 3.14
+user_age =25
+user_lacation = "New York"
+max_limit = 1000
+
+
+
+# Problem 3
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+
+variable_a = ( "string" )
+print = ( "variable_a" )
+
+variable_b = ( "int" )
+print = ( "variable_b" )
+
+variable_c = ( "float" )
+print = ( "variable_c" )
+
+variable_d = ( "bool" )
+print = ( "variable_d" )
+
+
+# Problem 4
+#How much is it for each of the three items listed?
+
+input = ( "bread" )
+print = ( "4.99" )
+input = ( "butter" )
+print = ( "2.99" )
+input = ( "sugar" )
+print = ( "3.99" )
+
+
